09118d8b62f2d9393fe12c68e0ac9fd1b1630608
sowjanya1308 <sowjanyasankara@gmail.com>
2023-10-03 11:28:29 +0530

    state tourism completed

---
+++ client/src/constants/static_data.js
@@ -0,0 +1,107 @@
+import hampi from "../assets/images/hampi.png";
+import coorg from "../assets/images/coorg.png"
+import mysore from "../assets/images/mysore.png"
+import udupi from "../assets/images/udupi.png"
+import chikmagalur from "../assets/images/chikmagalur.png"
+import jogfalls from "../assets/images/jogfalls.png"
+import murudeswar from "../assets/images/murudeswar.png"
+import banglore from "../assets/images/bangalore.png"
+import nandihills from "../assets/images/nandihills.png"
+import mangalore from "../assets/images/mangalore.png"
+
+
+class StaticData{
+    static StateListdata=[
+        {
+            title: "Hampi",
+            img: hampi,
+            description: 'Hampi is famous for its ruins which belong to the Vijayanagar Empire. This UNESCO World Heritage Site is also renowned for its group of monuments. Surrounded by 500 ancient monuments, temples, buildings, and captivating ruins',
+            besttime: 'October',
+            tourist: '35 Tourist Attractions',
+            stars: '⭐⭐⭐⭐⭐',
+        },
+        {
+            title: "Coorg",
+            img: coorg,
+            description: 'Coorg, officially known as Kodagu, is the most affluent hill station in Karnataka. It is well known for its breathtakingly exotic scenery and lush greenery. Forest covered hills, sallepye and coffee plantations only add to the landscape. ',
+            besttime: 'November',
+            tourist: '30 Tourist Attractions',
+            stars: '⭐⭐⭐⭐⭐',
+            
+        },
+        {
+            title: "Mysore",
+            img: mysore,
+            description: 'Mysore was one of the three largest Princely States in the erstwhile British Empire of India. To this day, the Mysore Palace stands tall as one of the most spectacular palaces in India.',
+            besttime: 'All over the year',
+            tourist: '36 Tourist Attractions',
+            stars: '⭐⭐⭐⭐⭐',
+            
+        },
+        {
+            title: "Udupi",
+            img: udupi,
+            description: 'Udupi is a coastal town in Karnataka, most famous for its vegetarian cuisine and South Indian restaurants all over the country. With beautifully carved ancient temples, laidback beaches and unexplored forests, ',
+            besttime: 'October',
+            tourist: '31 Tourist Attractions',
+            stars: '⭐⭐⭐⭐⭐',
+            
+        },
+        {
+            title: "Chikmagalur",
+            img: chikmagalur,
+            description: 'Popularly known as the ‘Coffee Land of Karnataka’, Chikmagalur is situated in the foothills of the Mullayangiri Range and is one of the most beautiful hill stations in Karnataka. Coffee lingering in the air with it plenty of coffee plantations.',
+            besttime: 'All over the year',
+            tourist: '22 Tourist Attractions',
+            stars: '⭐⭐⭐⭐⭐',
+            
+        },
+        {
+            title: "Jog Falls",
+            img: jogfalls,
+            description: 'Jog Falls is a major tourist attraction in Karnataka and is the highest waterfall in the state. Second, to the Nohkalikai Falls of Meghalaya, Jog falls drops about a huge 253 m (850 ft. ) in a single fall.',
+            besttime: 'July',
+            tourist: '9 Tourist Attractions',
+            stars: '⭐⭐⭐⭐',
+            
+        },
+        {
+            title: "Murudeswar",
+            img: murudeswar,
+            description: 'Murudeshwar is home to the second tallest statue of Lord Shiva (123 ft) in the world. With the shimmering Arabian Sea on three sides and the magnificent Western Ghats imposing their presence on this town.',
+            besttime: 'October',
+            tourist: '8 Tourist Attractions',
+            stars: '⭐⭐⭐⭐⭐',
+            
+        },
+        {
+            title: "Bangalore",
+            img: banglore,
+            description: 'Bangalore is loved for its pleasant weather, beautiful parks and the many lakes here. Bangalore is renowned for its eateries, street food corners, quirky cafes, coffee roasters and pubs dotting every corner of the city, serving cuisines from all over the world.',
+            besttime: 'All over the year',
+            tourist: '103 Tourist Attractions',
+            stars: '⭐⭐⭐⭐⭐',
+            
+        },
+        {
+            title: "Nandi Hills",
+            img: nandihills,
+            description: 'Nandi Hills is one such tourist spot that has gradually been discovered by visitors over the years and has now become a well-known weekend getaway. Nandi Hills is scattered with shrines and monument and is surrounded by mesmerizing views',
+            besttime: 'Decenmber',
+            tourist: '12 Tourist Attractions',
+            stars: '⭐⭐⭐⭐⭐',
+            
+        },
+        {
+            title: "Mangalore",
+            img: mangalore,
+            description: 'Located between the Arabian Sea and the Western Ghats, the port city of Mangalore or Mangaluru is known for its swaying coconut palms, beautiful beaches and temple architecture. ',
+            besttime: 'All over the year',
+            tourist: '35 Tourist Attractions',
+            stars: '⭐⭐⭐⭐⭐', 
+            
+        }
+    ];
+}
+
+export default StaticData;
---
+++ client/src/screens/StateTourism/components/StateCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+
+// import allepy from "../../../assets/images/allepy.png";
+// import CardBody from "react-bootstrap/esm/CardBody";
+// import { alignPropType } from "react-bootstrap/esm/types";
+
+
+const StateCard = (props) => {
+    
+    return (
+        <div className="mb-5 custom-cursor">
+            <Card className="cards" style={{ width: '58rem', height: '15rem', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <Card.Img className="allepyimg" variant="top" src={props.img} style={{ flex: '0 0 auto', width: '200px', height: 'auto', marginRight: '20px' }} />
+                <div style={{ flex: '1' }}>
+                    <Card.Body>
+                        <Card.Title className="fs-3">{props.title}</Card.Title>
+                    </Card.Body>
+                    <Card.Body>{props.tourist}</Card.Body>
+                    <Card.Body>{props.stars}</Card.Body>
+                </div>
+                <div style={{ flex: '2' }}>
+                    <Card.Body>
+                        <Card.Text style={{ marginRight: '50px', marginTop: '25px' }}>{props.description}</Card.Text>
+                    </Card.Body>
+                    <Card.Body>
+                        <Card.Text style={{ textAlign: 'right' }}><b>Best Time To Visit:</b></Card.Text>
+                        <Card.Text style={{ textAlign: 'right' }}>{props.besttime}</Card.Text>
+                    </Card.Body>
+                </div>
+            </Card>
+        </div>
+    );
+}
+
+export default StateCard;
