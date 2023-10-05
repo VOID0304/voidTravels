3e42270067b71218238bfbe091c228cb58cbe412
Geswar003 <geswar003@gmail.com>
2023-10-05 11:18:34 +0530

    changes by eswar

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,11 @@ import React from "react";
 import {Route, Routes, Navigate} from "react-router-dom";
 import SignIn from "./screens/login/signIn";
 import HomePage from "./screens/homepage/home";
-import StringConstants from "./constants/string_constants";
 import StateTourism from "./screens/StateTourism/stateTourism";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import Flights from "./screens/appbar options/flights";
 import SpecialPackages from "./screens/SpecialPackages/specialPackages";
 import MakemyPlan from "./screens/MakeMyPlan/makeMyPlan";
-import FanTopDestination from "./screens/fantopdestination/Fantopdestinations";
-import IconList from "./screens/SpecialPackages/components/icons";
-
 
 const App=()=> {
   return (
@@ -21,12 +16,9 @@ const App=()=> {
         <Route path="/login" Component={SignIn}/>
         <Route path="*" element={<Navigate replace to={"/"}/>} />
         <Route path="/stateTourism" Component={StateTourism}/>
-        <Route path="/flights" Component={Flights}/>
         <Route path="/specialPackages" Component={SpecialPackages}/>
         <Route path="/makemyplan" Component={MakemyPlan}/>
-        <Route path="/icon" Component={IconList}/>
       </Routes>
-      {/* <h4>{StringConstants.description_routes}</h4> */}
     </>
   );
 }
